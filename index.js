bf916b93968a41287b03356b215958a521b483a7
lovinugly <sousalaura7639@gmail.com>
2025-07-25 15:34:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+//atv 3 das notas
+
+//const nota1 = 3.4;
+//const nota2 = 2.3;
+//const nota3 = 4.4;
+
+//let somaDasNotas = nota1 + nota2 + nota3;
+
+//let media = somaDasNotas / 3;
+
+//console.log("As notas são:");
+//console.log("nota 1: " + nota1);
+//console.log("Nota 2: " + nota2);
+//console.log("Nota 3: " + nota3);
+//console.log("A média das notas é:" + media.toFixed(2));
+
+// atv 4 dos lados do quadrado
+
+//const lado = 10; 
+
+//let area = lado * lado; 
+
+//console.log(`O lado do quadrado é: ${lado}`);
+//console.log(`A área do quadrado é: ${area}`);
+
+//atv 5 do triângulo
+
+//const base = 20;
+//const altura = 10;
+
+//let area = (base * altura) / 2;
+//console.log(`A base do triângulo é: ${base}`);
+//console.log(`A altura do triângulo é: ${altura}`);
+//console.log(`A área do triângulo é: ${area}`);
+
+//atv 6 do circulo 
+
+//const PI = Math.PI;
+//let raio = 5,
+//let area = PI * (raio * raio)
+//console.log(`O raio do círculo é: ${raio}`);
+//console.log(`A área do círculo é: ${area}`);
+
+
+const PI = Math.PI;
+let raio = 5;
+let area = PI * (raio * raio);
+
+console.log(`O raio do círculo é: ${raio}`);
+console.log(`A área do círculo é: ${area}`);
+
